057eefc50882e5ad649bcd9a1508dd7752a1f925
Jose Vicente <jantvb@gmail.com>
2023-06-02 09:40:41 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'webworker-test';
+  factorialResult!: number;
+  factorialInput: number = 1;
+  loading: boolean = false;
+
+  constructor() {
+    
+  }
+
+  calculateFactorial() {
+    this.loading = true;
+    if (typeof Worker !== 'undefined') {
+      // Create a new
+      const worker = new Worker(new URL('./calc-factorial.worker', import.meta.url));
+      worker.onmessage = ({ data }) => {
+        this.factorialResult = data;
+        this.loading = false;
+      };
+      worker.postMessage(this.factorialInput);
+    } else {
+      // Web Workers are not supported in this environment.
+      // You should add a fallback so that your program still executes correctly.
+    }
+  }
 }
